97c6db92b3b03f3b8550a04e4a1f0056ce458a16
Jserin <jsr3643@naver.com>
2023-07-11 03:16:43 +0900

    페이징하다말았음

--- src/test/java/com/ll/sbb1/Sbb1ApplicationTests.java
+++ src/test/java/com/ll/sbb1/Sbb1ApplicationTests.java
@@ -4,6 +4,7 @@ import com.ll.sbb1.Answer.Answer;
 import com.ll.sbb1.Answer.AnswerRepository;
 import com.ll.sbb1.Question.Question;
 import com.ll.sbb1.Question.QuestionRepository;
+import com.ll.sbb1.Question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,37 +18,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class Sbb1ApplicationTests {
 
 	@Autowired
-	private QuestionRepository questionRepository;
-
-	@Autowired
-	private AnswerRepository answerRepository;
-
-	@Test
-	void contextLoads() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
-	}
+	private QuestionService questionService;
 
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		for (int i = 1; i <= 100; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}
 
 }
